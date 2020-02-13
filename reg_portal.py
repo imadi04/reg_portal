56e9423915eff21f921fd45e6afca963e706149e
Aditya Prakash <adityaprakash5050@gmail.com>
2020-02-13 20:52:30 +0530

    Generated GUI and posted user's reg details to database

--- reg_portal.py
+++ reg_portal.py
@@ -1,16 +1,36 @@
 import mysql.connector
+from tkinter import *
+import requests
 class Zomato:
     def __init__(self):
         self.conn=mysql.connector.connect(host="localhost",user="root",password="",database="hit")
         self.mycursor=self.conn.cursor()
-        self.user_menu()
+        self.root=Tk()
+        self.root.title("Registration Portal")
+        self.root.minsize(400,600) #for fixing min height of window
+        self.root.maxsize(400,900) #these two lines fix the window size.
+        self.root.configure(background="#0089ae")# for changing window background-color.
+        self.label1=Label(self.root,text="Registration Portal",bg="#0089ae",fg="#ffffff")
+        self.label1.configure(font=("Constantia",22,"bold"))
+        self.label1.pack(pady=(30,10)) #padding-left(x) and top(y)
 
-    def user_menu(self):
-        user_input=input("""How would you like to proceed?
+        self.label2=Label(self.root,text="""How would you like to proceed?
 1.Enter 1 to register
 2.Enter 2 to login
 3.Anything else to exit
-""")
+""",bg="#0089ae",fg="#ffffff")
+        self.label2.configure(font=("Constantia",12,"bold"))
+        self.label2.pack(pady=(30,10)) #padding-left(x) and top(y)
+
+        self.user_input=Entry(self.root) #for creating input field
+        self.user_input.pack(ipadx=5,ipady=10) #for changing the size of input/entry box x:width, y:height
+        self.click=Button(self.root,text="Enter",width=30,height=2,command=lambda:self.user_menu())
+        self.click.pack(pady=(15,15))
+        #self.user_menu()
+        self.root.mainloop()
+
+    def user_menu(self):
+        user_input=self.user_input.get()
         if user_input=='1':
             self.register()
         elif user_input=='2':
@@ -20,11 +40,22 @@ class Zomato:
 
 
     def register(self):
-        name=input("Enter name:")
-        email=input("Enter email:")
-        password=input("Enter password:")
-        self.mycursor.execute("INSERT INTO users(user_id,name,email,password) VALUES(NULL,'{}','{}','{}')".format(name,email,password))
-        self.conn.commit()
+        self.label1=Label(self.root,text="Name:",bg="#0089ae",fg="#ffffff")
+        self.label1.configure(font=("Constantia",12,"bold"))
+        self.name=Entry(self.root)
+        self.name.pack(ipadx=5,ipady=10)
+        print(self.name.get())
+        self.label2=Label(self.root,text="Email:",bg="#0089ae",fg="#ffffff")
+        self.label2.configure(font=("Constantia",12,"bold"))
+        self.email=Entry(self.root)
+        self.email.pack(ipadx=5,ipady=10)
+        self.label3=Label(self.root,text="Password:",bg="#0089ae",fg="#ffffff")
+        self.label3.configure(font=("Constantia",12,"bold"))
+        self.password=Entry(self.root)
+        self.password.pack(ipadx=5,ipady=10)
+        self.click=Button(self.root,text="Enter",width=30,height=2,command=lambda:[self.mycursor.execute("INSERT INTO users(user_id,name,email,password) VALUES(NULL,'{}','{}','{}')".format(self.name.get(),self.email.get(),self.password.get())),self.conn.commit()])                                                                                                 
+        self.click.pack(pady=(15,15))
+        
         print("Registration successful")
 
     def login(self):
@@ -35,7 +66,9 @@ class Zomato:
         if len(self.mycursor.fetchall())==0:
             print("Incorrect credentials")
         else:
-            print("Welcome!!")
+            username=email.split('@')
+            username=username[0]
+            print("Welcome {}!!".format(username))
             
 obj=Zomato()
 
